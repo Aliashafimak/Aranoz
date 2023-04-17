c20391c5a565ed603f95922c6604de748870c6d6
Your Name <aliashafimak@gmail.com>
2023-04-17 15:22:28 +0530

    added media in .env

--- app/views.py
+++ app/views.py
@@ -3,19 +3,28 @@ from store.models import Product
 from category.models import Category
 import random
 
-def home(request):
-    products = Product.objects.all().filter(is_available=True)
-    categories = Category.objects.all()
-    all_products = Product.objects.all()
-    random_products = random.sample(list(all_products), 5)
-    print(random_products)
-    context = {
-        'products': products,
-        'categories': categories,
-        'random_products': random_products
+# def home(request):
+#     products = Product.objects.all().filter(is_available=True)
+#     categories = Category.objects.all()
+#     all_products = Product.objects.all()
+#     random_products = random.sample(list(all_products), 5)
+#     print(random_products)
+#     context = {
+#         'products': products,
+#         'categories': categories,
+#         'random_products': random_products
 
+#     }
+#     return render(request, 'index.html', context)
+def home(request):
+    all_products = Product.objects.all()
+    random_products = []
+    if len(all_products) >= 5:
+        random_products = random.sample(list(all_products), 5)
+    context = {
+        'random_products': random_products
     }
-    return render(request, 'index.html', context)
+    return render(request, 'app/index.html', context)
 
 
 
