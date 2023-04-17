44cd0824549568e08e793409372c7d80ba6e0efa
Your Name <aliashafimak@gmail.com>
2023-04-17 13:46:33 +0530

    added media in .env

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -136,15 +136,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL ='/static/'
-STATIC_ROOT =BASE_DIR/'static'
-STATICFILES_DIRS=[
-    'ecommerce/static/'
-]     
+if DEBUG==True:
+    STATIC_URL ='/static/'
+    STATIC_ROOT =BASE_DIR/'static'
+    STATICFILES_DIRS=[
+        'ecommerce/static/'
+    ]     
+else:
+    STATIC_URL = '/static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_DIRS = os.path.join(BASE_DIR, 'ecommerce/static/')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR /'media'
-
+if DEBUG==True:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR /'media'
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = '/home/ubuntu/myprojectdir/Aranoz/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
