6e202664f1a0c0299639edc97cf55b2e5cb0898a
Your Name <aliashafimak@gmail.com>
2023-04-17 15:12:57 +0530

    updated static settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,6 +164,7 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
+ADMINS = [('alia', 'aliashafimak@gmail.com'),]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT=587 
