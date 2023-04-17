407984cfab1292ce75d89b9fe3dac9708e2c60f6
Your Name <aliashafimak@gmail.com>
2023-04-17 14:19:24 +0530

    new

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -164,8 +164,9 @@ MESSAGE_TAGS = {
 }
 
 # SMTP configuration
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = 587 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT=587 
 EMAIL_HOST_USER = env("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
